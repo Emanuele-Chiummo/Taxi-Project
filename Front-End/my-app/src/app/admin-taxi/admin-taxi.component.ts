334f2b216fc881f0644bad3870dc2a2b794464b1
Emanuele Chiummo <98900059+Emanuele-Chiummo@users.noreply.github.com>
2024-01-22 17:23:36 +0100

    select box modal add taxi

--- Front-End/my-app/src/app/admin-taxi/admin-taxi.component.ts
+++ Front-End/my-app/src/app/admin-taxi/admin-taxi.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaxiServicesService } from '../services/taxi-services.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface User {
+  name: string;
+  surname: string;
+  // Add other properties as needed
+}
+
+
 
 @Component({
   selector: 'app-admin-taxi',
   templateUrl: './admin-taxi.component.html',
   styleUrl: './admin-taxi.component.css'
 })
-export class AdminTaxiComponent {
+export class AdminTaxiComponent implements OnInit{
+
+
+  users: any[] = [];
 
   taxi: any[] = []
 
   ngOnInit(): void {
 
+    this.getAllUser();
     this.getAlltaxi();  
 
   }
 
-  constructor(private ts: TaxiServicesService) { }
+  constructor(private ts: TaxiServicesService, private fb: FormBuilder) { }
 
   getAlltaxi() {
     this.ts.getAllTaxi().subscribe(x => {
@@ -24,4 +37,16 @@ export class AdminTaxiComponent {
     })
   }
 
+  getAllUser(): void {
+    this.ts.getAllUser().subscribe((users: User[]) => {
+        this.users = users;
+        console.log(this.users);
+    });
 }
+
+}
+
+
+
+
+
